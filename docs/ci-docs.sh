7290d53182c147c9912e59f06a3c30276537dda9
James Strachan <james.strachan@gmail.com>
2016-09-05 11:52:20 +0100

    add generated site

---
+++ docs/ci-docs.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+echo ============================================
+echo Deploying fabric8-spring documentation
+echo ============================================
+
+cd doc && \
+mvn -Phtml,pdf package && \
+git clone -b gh-pages https://jstrachan:${GITHUB_TOKEN}@github.com/fabric8io/fabric8-spring.git gh-pages && \
+git config --global user.email "travis@fabric8.io" && \
+git config --global user.name "Travis" && \
+cp -rv target/generated-docs/* gh-pages/ && \
+cd gh-pages && \
+mv index.pdf fabric8-spring.pdf && \
+git add --ignore-errors * && \
+git commit -m "generated documentation" && \
+git push origin gh-pages && \
+cd .. && \
+rm -rf gh-pages target
